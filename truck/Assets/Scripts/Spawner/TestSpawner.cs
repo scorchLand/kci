8fa1c7b254e5e6aa5da78883c1c4a056108eb26b
kci2626 <kci2626123@gmail.com>
2024-05-26 18:13:20 +0900

    메인 씬 수정

--- truck/Assets/Scripts/Spawner/TestSpawner.cs
+++ truck/Assets/Scripts/Spawner/TestSpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TestSpawner : MonoBehaviour
 {
-    public Hero hero;
+    public Hero[] hero;
     private void Start()
     {
         StartCoroutine(RoutineSpawn());
@@ -14,7 +14,7 @@ public class TestSpawner : MonoBehaviour
         while(true)
         {
             yield return new WaitForSeconds(Random.Range(0,5));
-            var target = Instantiate(hero);
+            var target = Instantiate(hero[Random.Range(0,hero.Length)]);
             target.transform.position = new Vector3(Random.Range(-2f, 2f), -7, 0);
         }
     }
