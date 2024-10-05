c79360f9d32279ce915b310d20df0bd3ef7886bf
kci2626 <kci2626123@gmail.com>
2024-10-05 14:19:32 +0900

    테이블 임시코드 ----------------------------------

---
+++ truck/Assets/Scripts/Tables/Partial/RowSpwaner.cs
@@ -0,0 +1,11 @@
+
+namespace Grooz
+{
+    public partial class RowSpwaner
+    {
+        public float AppearanceDistance;
+        public float NextAppearanceDistance;
+        public int AppearanceIndex;
+        public float Speed;
+    }
+}
