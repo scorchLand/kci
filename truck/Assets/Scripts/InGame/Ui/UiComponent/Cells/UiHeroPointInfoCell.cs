f4a3d70116e9d3cbcfab1165346dc091c7fe2bbe
kci2626 <kci2626123@gmail.com>
2025-04-24 22:41:05 +0900

    기능 재적용

--- truck/Assets/Scripts/InGame/Ui/UiComponent/Cells/UiHeroPointInfoCell.cs
+++ truck/Assets/Scripts/InGame/Ui/UiComponent/Cells/UiHeroPointInfoCell.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiHeroPointInfoCell : UiComponent
 {
-    public GameObject goGauge;
+    public Slider goGauge;
     //public TextMeshProUGUI tmpCount;
     public TextMeshProUGUI tmpValue;
 
     private void Update()
     {
-        goGauge.transform.localScale = new Vector3(InGameController.Instance.stageController.ScoreList[0].Value, 1, 1);
+        goGauge.value = InGameController.Instance.stageController.ScoreList[0].Value;
         //tmpCount.text = $"{InGameController.Instance.stageController.ScoreList[0].Count}";
         tmpValue.text = $"{(int)(InGameController.Instance.stageController.ScoreList[0].Value * 100)}%";
     }
